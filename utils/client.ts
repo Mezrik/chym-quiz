b2499705f7483863999473386b63e2ac9c1cacb8
Martin Petr <martin.petr.276@gmail.com>
2024-06-25 01:38:36 +0200

    fix: rename supabase env variables

--- utils/client.ts
+++ utils/client.ts
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 
-import { createBrowserClient } from "@supabase/ssr"
-import type { SupabaseClient } from "@supabase/supabase-js"
+import { createBrowserClient } from "@supabase/ssr";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
-import type { Database } from "../types/database"
+import type { Database } from "../types/database";
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  )
+    process.env.NEXT_SUPABASE_URL!,
+    process.env.NEXT_SUPABASE_ANON_KEY!
+  );
 }
